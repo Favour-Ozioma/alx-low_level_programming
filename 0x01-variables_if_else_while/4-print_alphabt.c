2231f5e2045df9402f5c42eb4923618acf9f5eb0
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-10 15:50:39 +0100

    achicha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * * main - print if the number is postive, zero, or negative
+ * *
+ * * Description: using the main function
+ * * this program prints "Programming is positive, zero, or negative
+ * * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int e;
+	int q;
+
+	e= 'e';
+	q= 'q';
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	  /* your code goes there */
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		if (n!=e && n!=q);
+		putchar (n);
+	}
+	putchar('\n');
+	return (0);
+}

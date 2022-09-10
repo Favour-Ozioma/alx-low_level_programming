577347eea9fb73c7a158163aa87aa98acccef774
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-10 18:07:41 +0100

    achicha

---
+++ 0x01-variables_if_else_while/6-print_munberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * * main - print numbers 0 to 9
+ * *
+ * * Description: using the main function
+ * * this program prints "0,1,2,3,4,5,6,7,8,9"
+ * * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar("%d", n);
+	putchar("\n");
+	return (0);
+}

789c42f59addadeb9179d87555022c2632b29640
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-11 04:01:22 +0100

    achicha

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,8 +8,7 @@
  *    * * Description: using the main function
  *     * * this program prints "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F"
  *      * * Return: 0
- *      
- *      */
+ */
 int main(void)
 {
 	int n;

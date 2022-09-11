6d59af2ec68a028279e1ba519a5e12ccc6d50de1
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-11 04:46:47 +0100

    achicha

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 /*
  * File: 100-print_comb3.c
- * Auth: Brennan D Baraban
- */
+ * Auth: Favour D Ozioma
 
 #include <stdio.h>
 
@@ -24,7 +23,6 @@ int main(void)
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}

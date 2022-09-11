d9c1bfe6c19802430d403fa8237c12a02ef7ca66
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-11 02:34:26 +0100

    achicha

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  * * main - print all the numbers of base 16 in lowercase, followed by a new line.
+ *   * *
+ *    * * Description: using the main function
+ *     * * this program prints "0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F"
+ *      * * Return: 0
+ *       */
+int main(void)
+{
+	int n;
+			
+	for (n = 48; n < 70; n++)
+	{
+		if (n >= 57 && n >= 65)
+		{
+			putchar (n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

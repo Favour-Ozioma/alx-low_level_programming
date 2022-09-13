6c62280775989c22a3aeb8179ef225c01bb46ec6
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-13 20:19:18 +0100

    ezi_offia

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+/*
+ * Auth: Favour D Ozioma
+ */
+
+#include "main.h"
+
+
+/**
+ * main - Prints the alphabet in lower case
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void);
+{
+	for(faozio = 'a', faozio <= z; faozio++)
+		_putchar(faozio);
+		_putchar('\n');
+
+	return (0);
+
+}

81a66a06f112ae1cc3d0e6b46e7d6a61bfec07c2
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-13 11:58:33 +0100

    ezi_offia

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * Auth: Favour D Ozioma
+ */
+
+#include "main.h"
+#include "_putchar.c"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	int _putchar() = 98;
+
+	return(0);
+}

4ee8d692a6d0ed61feca38db93e24ec4f1e7af6e
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-14 08:50:59 +0100

    agbugbu

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,16 @@
 
 
 /**
- * print_last_digit - Prints the last digit of a number
+ * print_last_digit - Prints the last digit of a number.
  *
- * Return: The last digit of the number
+ * @n: The number in question.
+ * Return: The last digit of the number.
  */
 
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
+	
 	if (last_digit < 0)
 		last_digit *= -1;
 

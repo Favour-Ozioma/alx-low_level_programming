576fce0f73a033c6b923016f7e158f70e7a5d5ba
Favour Ozioma <Favour_Ozioma@outlook.com>
2022-09-14 07:07:16 +0100

    agbugbu

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+/*
+ * Auth: Favour D Ozioma
+ */
+
+#include "main.h"
+
+
+/**
+ * int _abs - Computes the absolute value of an integer
+ *
+ * @c: The character to be checked.
+ *
+ * Return: Always the absolute.
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	else
+		return (-c);
+}

05501914442e4608e7f591e176026419648a23db
root <root@Faozio.localdomain>
2022-09-22 04:00:47 +0100

    oke_amu

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _strcat - concatenates the string pointed to by @src to
  * the end of the string pointed to by @dest
@@ -11,13 +12,13 @@
 char *_strcat(char *dest, char *src)
 {
 
-	int index = 0, dest_len = 0;
+        int index = 0, dest_len = 0;
 
-	while (dest[index++])
-		dest_len++;
+        while (dest[index++])
+                dest_len++;
 
-	for (index = 0; src[index]; index++)
-		[dest_len++] = src[index];
+        for (index = 0; src[index]; index++)
+                [dest_len++] = src[index];
 
-	return (dest);
+        return (dest);
 }

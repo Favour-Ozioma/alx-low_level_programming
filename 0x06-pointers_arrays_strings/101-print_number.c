94548ffebb8d3df77b0c50d2021dff8d421f9785
root <root@Faozio.localdomain>
2022-09-28 05:53:46 +0100

    critical push

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,22 +1,26 @@
 #include "main.h"
+
 /**
- *print_number - prints an integer.
- *only using the putchar function.
- *noarrays and pointers.
- *@n: integer to be printed.
- *Return: void.
- */
+  * print_number - print numbers chars
+  * @n: integer params
+  * Return: 0
+ **/
+
 void print_number(int n)
 {
-	unsigned int num = n;
-/*check if number is negative*/
+	unsigned int n1;
+
+	n1 = n;
+
 	if (n < 0)
 	{
-		_putchar('_');
-		num = -n;
+		_putchar('-');
+		n1 = -n;
 	}
-/* print number by recursion*/
-	if ((num / 10) > 0)
-		print_number(num / 10);
-	_putchar((num % 10) + '0');
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }

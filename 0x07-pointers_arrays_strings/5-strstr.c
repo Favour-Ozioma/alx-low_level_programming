446f244720a270e34fc5ce364ae9fe6e0ce682a5
root <root@Faozio.localdomain>
2022-09-28 05:12:13 +0100

    Oke_Offia

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
  * _strstr - locate a substring
@@ -9,24 +10,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0, j = 0;
-
-	while (haystack[1])
+	for (; *haystack != '\0'; haystack++)
 	{
-		while (needle[j] && (haystack[i] == needle[0]))
+		char *one = haystack;
+		char *two = needle;
+
+		while (*one == *two && *two != '\0')
 		{
-			if (haystack[i + j] == needle[0])
-				j++;
-			else
-				break;
+			one++;
+			two++;
 		}
-		if (needle[j])
-		{
-			i++;
-			j = 0;
-		}
-		else
-			return (haystack + i);
+		if (*two == '\0')
+			return (haystack);
 	}
-	return (0);
+
+	return (NULL);
 }
